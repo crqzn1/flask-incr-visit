eb10772d57adce636d64d1c9e04954ebcb99f91d
crqzn1 <c.r.q.z.n.1@gmail.com>
2021-12-04 01:36:14 +1100

    flask mysql docker init commit

--- app.py
+++ app.py
@@ -1,23 +1,32 @@
-import time
-
-import redis
 from flask import Flask
+import mysql.connector
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
 
 def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
+    config = {
+        'user': 'root',
+        'password': 'root',
+        'host': 'mysqldb',
+        'port': '3306',
+        'database': 'mydb',
+        'auth_plugin': 'mysql_native_password'
+    }
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    cursor.execute('SELECT max(hit_count) FROM hit_count_table')
+    result = cursor.fetchone()[0]
+    cursor.execute(f'INSERT INTO hit_count_table VALUES ({result+10})')
+    cursor.close()
+    connection.commit()
+    connection.close()
+
+    return result
 
 @app.route('/')
 def hello():
     count = get_hit_count()
     return 'Hello World from tttxxx! I have been seen {} times.\n'.format(count)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
---
+++ db/init.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE mydb;
+use mydb;
+
+CREATE TABLE hit_count_table (hit_count int);
+
+INSERT INTO hit_count_table
+VALUES (111), (222);
